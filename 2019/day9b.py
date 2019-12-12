42a034d75ded6eae2c9991eed6b58e14f31a5856
gliech <gliech@users.noreply.github.com>
2019-12-12 23:45:42 +0100

    corrected typo in day9b

--- 2019/day9b.py
+++ 2019/day9b.py
@@ -1,4 +1,4 @@
 from aocd import get_data
 from agc import AdventGuidanceComputer as comp
 
-print(list(comp(get_data(year=2019, day=9).split(','))([2]))[1])
+print(list(comp(get_data(year=2019, day=9).split(','))([2]))[0])
