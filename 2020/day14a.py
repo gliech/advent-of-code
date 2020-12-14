9250cbaa9cc8968703c86b69cd1418371860646b
gliech <gliech@users.noreply.github.com>
2020-12-14 10:17:56 +0100

    solved day14a

---
+++ 2020/day14a.py
@@ -0,0 +1,20 @@
+# Setup
+from aocd import get_data
+
+data = get_data(year=2020, day=14).split('\n')
+data = [tuple(map(int, find(r'(\d+)\D+(\d+)', l)[0])) if l[1]=='e' else find(r'[10X]+', l)[0] for l in data]
+
+#Solution
+from re import findall as find
+from collections import defaultdict
+
+memory = defaultdict(int)
+
+for line in data:
+    if type(line) is str:
+        true_mask = int(line.translate(str.maketrans('1X', '01')), base=2)
+        mask_fill = int(line.replace('X', '0'), base=2)
+    else:
+        memory[line[0]] = line[1] & true_mask | mask_fill
+
+print(sum(memory.values()))
