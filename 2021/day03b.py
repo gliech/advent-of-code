ba957a3ad03345d402943d086c3b51b48b07f272
gliech <gliech@users.noreply.github.com>
2021-12-03 22:54:30 +0100

    2021 day03b

--- 2021/day03b.py
+++ 2021/day03b.py
@@ -1,28 +1,17 @@
 # Setup
 from aocd import get_data
-import numpy as np
 from collections import Counter
-from operator import mul
+from operator import mul, itemgetter
 from functools import partial
 data = get_data(year=2021, day=3).split('\n')
-data = np.array(list(map(tuple, data)))
 
-def common(a, b):
-    for idx in range(len(a)):
-        if a[idx] != b [idx]:
-            return idx
+def sieve(data, bitcrit, idx=0):
+    if len(data) == 1:
+        return int(data[0], base=2)
+    else:
+        acc = Counter(map(itemgetter(idx), data))
+        bit = bitcrit(map(partial(bitcrit, key=acc.get), [acc, reversed(acc)]))
+        return sieve([x for x in data if x[idx] == bit], bitcrit, idx+1)
 
-# Longform
-def long_solution(data):
-    a = [''.join(x) for x in np.apply_along_axis(lambda x: Counter(x).most_common(),0,data)[:, 0]]
-    b = map(lambda x: max(data, key=partial(common, x)), a)
-    return mul(*map(lambda x: int(''.join(x), base=2), b))
-
-# Golfed
-def golfed_solution(d):
-    return \
-d
-
-# Output
-solve=0
-print([long_solution, golfed_solution][solve](data))
+solution = mul(*(sieve(data, criterion) for criterion in (min, max)))
+print(solution)
