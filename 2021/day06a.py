b23b8e2b612bc57aea5f51079ce8f87e9cc3fa54
gliech <gliech@users.noreply.github.com>
2021-12-07 16:41:10 +0100

    simplified 2021/6

--- 2021/day06a.py
+++ 2021/day06a.py
@@ -1,16 +1,14 @@
 # Setup
 from aocd import get_data
-from collections import Counter, deque
+from collections import deque
 
 data = get_data(year=2021, day=6).split(',')
-data = Counter(int(n) for n in data)
 fish = deque([0]*9)
 
-for idx, val in data.items():
-    fish[idx] = val
+for val in data:
+    fish[int(val)] += 1
 
 for _ in range(256):
-    print(fish)
     fish.rotate(-1)
     fish[6] += fish[8]
 
