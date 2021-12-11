0c2d5c6d72a044419cd1e5433f3cf465a48e1a1d
gliech <gliech@users.noreply.github.com>
2021-12-11 10:01:04 +0100

    2021/11

---
+++ 2021/day11a.py
@@ -0,0 +1,22 @@
+from aocd import get_data
+import numpy as np
+import scipy.ndimage
+
+def flashed_at(a):
+    if  0 < a[4] <= 9:
+        return a[4] + len(a[a>9])
+    else:
+        return 0
+
+data = get_data(year=2021, day=11)
+data = np.asarray([[int(j) for j in i] for i in data.split('\n')])
+flashes = 0
+
+for _ in range(100):
+    data += 1
+    while len(data[data>9]):
+        flashes += len(data[data>9])
+        data = scipy.ndimage.generic_filter(data, flashed_at, size = 3,
+                mode='constant', cval=-1)
+
+print(flashes)
---
+++ 2021/day11b.py
@@ -0,0 +1,23 @@
+from aocd import get_data
+from itertools import count
+import numpy as np
+import scipy.ndimage
+
+def flashed_at(a):
+    if  0 < a[4] <= 9:
+        return a[4] + len(a[a>9])
+    else:
+        return 0
+
+def main_loop(data):
+    for i in count():
+        if np.alltrue(data==0):
+            return i
+        data += 1
+        while len(data[data>9]):
+            data = scipy.ndimage.generic_filter(data, flashed_at, size = 3,
+                    mode='constant', cval=-1)
+
+data = get_data(year=2021, day=11)
+data = np.asarray([[int(j) for j in i] for i in data.split('\n')])
+print(main_loop(data))
