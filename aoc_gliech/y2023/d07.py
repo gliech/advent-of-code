d7a8a0857df82cae5b2daadc7c848f221f1660ad
gliech <gliech@users.noreply.github.com>
2023-12-07 14:40:04 +0100

    2023/07b

--- aoc_gliech/y2023/d07.py
+++ aoc_gliech/y2023/d07.py
@@ -1,23 +1,22 @@
-import re
 from collections import Counter
 from operator import itemgetter
+from functools import partial
 
-def hand_value(hand):
-    htype = sum(value**2 for value in Counter(hand).values())
-    for idx, letter in enumerate("AKQJT"):
-        hand = hand.replace(letter, chr(69-idx))
-    return htype*16**5+int(hand,16)
+def count_for_b(hand):
+    hand = Counter(hand)
+    jokers = hand.pop("J", 0)
+    hand[next(iter(hand.most_common()), "J")[0]] += jokers
+    return hand
 
+def winnings(data, count_cards, card_order):
+    tie_val = lambda h: sum(list(card_order).index(c)*16**i for i, c in enumerate(reversed(h)))
+    hand_val = lambda h: (sum(value**2 for value in count_cards(h).values())<<20)+tie_val(h)
+    hands = ((hand_val(l.split()[0]), int(l.split()[1])) for l in data.split("\n"))
+    return sum((i+1)*v[1] for i, v in enumerate(sorted(hands, key=itemgetter(0))))
 
-def prep(data):
-    return ((hand_value(l.split()[0]), int(l.split()[1])) for l in data.split("\n"))
-
-def part_a(data):
-    return sum((i+1)*v[1] for i, v in enumerate(sorted(prep(data), key=itemgetter(0))))
-
-def part_b(data):
-    return None
+part_a = partial(winnings, count_cards=Counter, card_order="23456789TJQKA")
+part_b = partial(winnings, count_cards=count_for_b, card_order="J23456789TQKA")
 
 if __name__ == "__main__":
     from aocd import get_data
-    print(part_a(get_data(year=2023, day=7)))
+    print(part_b(get_data(year=2023, day=7)))
