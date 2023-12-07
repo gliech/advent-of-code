bab18feade4dcf486c3ecd7542d721e00d5e5845
gliech <gliech@users.noreply.github.com>
2023-12-07 16:45:43 +0100

    golf 2023/07

--- aoc_gliech/y2023/d07.py
+++ aoc_gliech/y2023/d07.py
@@ -1,21 +1,27 @@
 from collections import Counter
-from operator import itemgetter
+from operator import call
 from functools import partial
+from itertools import starmap
 
-def count_for_b(hand):
+# def winnings(counter, card_order, data):
+#     tie_value = lambda hand: sum(list(card_order).index(value)<<idx*4 for idx, value in enumerate(reversed(hand)))
+#     hand_value = lambda hand: (sum(value**2 for value in counter(hand).values())<<20)+tie_value(hand)
+#     hands = (starmap(call, zip((hand_value, int), line.split())) for line in data.split("\n"))
+#     return sum((rank+1)*next(bid) for rank, bid in enumerate(sorted(hands, key=next)))
+
+w = lambda c,o,d: sum((r+1)*next(b) for r, b in enumerate(sorted((starmap(call,
+                  zip((lambda h: (sum(g**2 for g in c(h).values())<<20)+sum(
+                  list(o).index(v)<<i*4 for i, v in enumerate(reversed(h))),
+                  int), l.split())) for l in d.split("\n")), key=next)))
+
+def jCounter(hand):
     hand = Counter(hand)
     jokers = hand.pop("J", 0)
-    hand[next(iter(hand.most_common()), "J")[0]] += jokers
+    hand[next(iter(hand.most_common()), "X")[0]] += jokers
     return hand
 
-def winnings(data, count_cards, card_order):
-    tie_val = lambda h: sum(list(card_order).index(c)*16**i for i, c in enumerate(reversed(h)))
-    hand_val = lambda h: (sum(value**2 for value in count_cards(h).values())<<20)+tie_val(h)
-    hands = ((hand_val(l.split()[0]), int(l.split()[1])) for l in data.split("\n"))
-    return sum((i+1)*v[1] for i, v in enumerate(sorted(hands, key=itemgetter(0))))
-
-part_a = partial(winnings, count_cards=Counter, card_order="23456789TJQKA")
-part_b = partial(winnings, count_cards=count_for_b, card_order="J23456789TQKA")
+part_a = partial(w, Counter, "23456789TJQKA")
+part_b = partial(w, jCounter, "J23456789TQKA")
 
 if __name__ == "__main__":
     from aocd import get_data
