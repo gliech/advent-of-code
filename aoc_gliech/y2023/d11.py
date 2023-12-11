fbf462da8b2c04e935b7e53c8ffa90a1a31f8451
Gregor Bückendorf <gregor.bueckendorf@cbc.de>
2023-12-11 14:34:39 +0100

    2023/11b

--- aoc_gliech/y2023/d11.py
+++ aoc_gliech/y2023/d11.py
@@ -1,6 +1,7 @@
 import numpy as np
 from itertools import combinations, starmap
 from operator import sub
+
 custom_data = """
         ....
         .#..
@@ -9,25 +10,25 @@ custom_data = """
         ...#"""
 
 def prep(data):
-    data = data.split('\n')
-    return np.array([list(line) for line in data], dtype='U1')
+    return np.array([list(line) for line in data.split('\n')], dtype='U1')
 
-def part_a(data):
-    image = prep(data)
+def expand(image):
     for axis in 0, 1:
         empty_spaces = np.where(np.all(image == ".", axis))[0]
         image = np.insert(image, empty_spaces, ".", axis=1-axis)
-    return sum(map(sum, map(np.abs, starmap(sub, combinations(map(np.array, zip(*np.where(image == "#"))), 2)))))
+    return image
+
+def galaxy_distances(image):
+    return map(sum, map(np.abs, starmap(sub, combinations(map(np.array, zip(*np.where(image == "#"))), 2))))
+
+def part_a(data):
+    return sum(galaxy_distances(expand(prep(data))))
 
 def part_b(data):
     image = prep(data)
-    print(image)
-    dist_past = map(sum, map(np.abs, starmap(sub, combinations(map(np.array, zip(*np.where(image == "#"))), 2))))
-    for axis in 0, 1:
-        empty_spaces = np.where(np.all(image == ".", axis))[0]
-        image = np.insert(image, empty_spaces, ".", axis=1-axis)
-    dist_now = map(sum, map(np.abs, starmap(sub, combinations(map(np.array, zip(*np.where(image == "#"))), 2))))
-    return starmap(lambda a, b: a+(b-a)*100, zip(dist_past, dist_now))
+    dist_past = galaxy_distances(image)
+    dist_now = galaxy_distances(expand(image))
+    return sum(starmap(lambda a, b: a+(b-a)*999999, zip(dist_past, dist_now)))
 
 if __name__ == "__main__":
     from aoc_gliech import solve
