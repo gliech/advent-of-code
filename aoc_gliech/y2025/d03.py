d1c1f79552c520eba53d1030592e3cca7ebd18d5
gliech <gliech@users.noreply.github.com>
2025-12-04 23:04:58 +0100

    refactor: reorganise code in day 1 and 3 of 2025

--- aoc_gliech/y2025/d03.py
+++ aoc_gliech/y2025/d03.py
@@ -3,7 +3,7 @@ from functools import partial
 def prep(data):
     return (tuple(map(int, line)) for line in data.split("\n"))
 
-def line_value(line, digits):
+def calc_line(digits, line):
     last_index, result = 0, 0
     for i in reversed(range(digits)):
         digit = max(line[last_index:len(line)-i])
@@ -11,18 +11,10 @@ def line_value(line, digits):
         result = result*10+digit
     return result
 
-def part_a(data):
-    return sum(map(partial(line_value, digits=2), prep(data)))
+def gen_part(digits):
+    return lambda d: sum(map(partial(calc_line, digits), prep(d)))
 
-def part_b(data):
-    return sum(map(partial(line_value, digits=12), prep(data)))
-
-
-# earlier solution for the central function of part a
-def line_value_part_a(line):
-    d1 = max(line[:-1])
-    d2 = max(line[line.index(d1)+1:])
-    return d1*10+d2
+part_a, part_b = map(gen_part, (2, 12))
 
 if __name__ == "__main__":
     from aoc_gliech import solve
